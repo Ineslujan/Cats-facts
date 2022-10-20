dfcb60f00c079d8cded4e5c5767bcbe2840618f3
Inès LUJAN <melusyne@iness-macbook-air.home>
2022-10-20 15:24:13 +0200

    same filter than facts in breeds

--- src/components/3-Organisms/Breeds/Breeds.js
+++ src/components/3-Organisms/Breeds/Breeds.js
@@ -8,12 +8,14 @@ import Main from '../Main/Main';
 import Loader from '../../1-Atoms/Loader/Loader';
 import CardBreed from '../../2-Molecules/CardBreed/CardBreed';
 import Pagination from '../../2-Molecules/Pagination/Pagination';
+import SearchForm from '../../2-Molecules/SearchForm/SearchForm';
 
 function Breeds() {
   const [isLoading, setIsLoading] = useState(true);
   const [datasApi, setDatasApi] = useState([]);
   const [currentPage, setCurrentPage] = useState(null);
   const [lastPage, setLastPage] = useState(null);
+  const [searchValue, setSearchValue] = useState('');
 
   const getBreedsDatas = async (newPage) => {
     setIsLoading(true);
@@ -34,12 +36,24 @@ function Breeds() {
     <Main>
       {isLoading ? <Loader /> : (
         <>
+          <SearchForm searchValue={searchValue} setSearchValue={setSearchValue} />
           <section className="breeds">
-            {datasApi && datasApi.map((d) => (
+            {datasApi && datasApi.filter((data) => (
+              data.breed.toLowerCase().includes(searchValue.toLowerCase())
+              || data.country.toLowerCase().includes(searchValue.toLowerCase())
+              || data.origin.toLowerCase().includes(searchValue.toLowerCase())
+              || data.coat.toLowerCase().includes(searchValue.toLowerCase())
+              || data.pattern.toLowerCase().includes(searchValue.toLowerCase())
+            )).map((d) => (
               <CardBreed key={uuid()} breed={d} />
             ))}
           </section>
-          <Pagination currentPage={currentPage} lastPage={lastPage} getDatas={getBreedsDatas} />
+          <Pagination
+            currentPage={currentPage}
+            lastPage={lastPage}
+            getDatas={getBreedsDatas}
+            setSearchValue={setSearchValue}
+          />
         </>
       )}
     </Main>
